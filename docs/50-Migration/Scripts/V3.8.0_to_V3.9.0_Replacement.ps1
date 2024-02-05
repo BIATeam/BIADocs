b837cfb491982177a93a730646762801eb9c65e2
Gwenael Fillon <Gwenael.fillon@free.fr>
2024-02-05 17:23:35 +0100

    Add sequence to insert function

--- docs/50-Migration/Scripts/V3.8.0_to_V3.9.0_Replacement.ps1
+++ docs/50-Migration/Scripts/V3.8.0_to_V3.9.0_Replacement.ps1
@@ -1,5 +1,6 @@
-$Source = "D:\Source\GitHub\BIATeam\BIADemo";
+$Source = "C:\Sources\Github.com\BIATeam\BIADemo";
 $SourceBackEnd = $Source + "\DotNet"
+$SourceFrontEnd = $Source + "\Angular"
 
 $ExcludeDir = ('dist', 'node_modules', 'docs', 'scss', '.git', '.vscode', '.angular')
 
@@ -56,6 +57,130 @@ function GetPresentationApiFolder {
     return $null
   }
 }
+
+function InsertFunctionInClass() {
+  param (
+    [string]$Source,
+    [string]$MatchBegin,
+    [string]$FunctionBody,
+    [string]$ReplaceByExpr,
+    [string]$NoMatchCondition,
+    [string]$MatchCondition,
+    [string[]]$ReplaceSeqences,
+    [string[]]$ReplaceByMatch1
+  )
+  foreach ($childDirectory in Get-ChildItem -Force -Path $Source -Directory -Exclude $ExcludeDir) {
+    InsertFunctionInClassRec -Source $childDirectory.FullName -MatchBegin $MatchBegin -NoMatchCondition $NoMatchCondition -FunctionBody $FunctionBody -ReplaceByExpr $ReplaceByExpr -MatchCondition $MatchCondition -ReplaceSeqences $ReplaceSeqences -ReplaceByMatch1 $ReplaceByMatch1
+  }
+}
+
+function InsertFunctionInClassRec() {
+  param (
+    [string]$Source,
+    [string]$MatchBegin,
+    [string]$FunctionBody,
+    [string]$ReplaceByExpr,
+    [string]$NoMatchCondition,
+    [string]$MatchCondition,
+    [string[]]$ReplaceSeqences,
+    [string[]]$ReplaceByMatch1
+  )
+  foreach ($childDirectory in Get-ChildItem -Force -Path $Source -Directory -Exclude $ExcludeDir) {
+    InsertFunctionInClassRec -Source $childDirectory.FullName -MatchBegin $MatchBegin -NoMatchCondition $NoMatchCondition -FunctionBody $FunctionBody -ReplaceByExpr $ReplaceByExpr -MatchCondition $MatchCondition -ReplaceSeqences $ReplaceSeqences -ReplaceByMatch1 $ReplaceByMatch1
+  }
+
+  $fichiersTypeScript = Get-ChildItem -Path $Source -Filter "*.ts"
+  foreach ($fichier in $fichiersTypeScript) {
+    $contenuFichier = Get-Content -Path $fichier.FullName -Raw
+
+    # Vérifiez si la classe hérite de CrudItemService
+    if ($contenuFichier -match $MatchBegin) {
+      $nomClasse = $matches[1]
+      if ($MatchCondition -eq "" -or $contenuFichier -match $MatchCondition)
+      {
+        # Vérifiez si les fonctions ne sont pas déjà présentes
+        if ($contenuFichier -notmatch $NoMatchCondition) {
+          # Utilisez une fonction pour trouver la position de la fermeture de la classe
+          $positionFermetureClasse = TrouverPositionFermetureClasse $contenuFichier $MatchBegin
+
+          $FunctionBodyRep = $FunctionBody;
+          For($i=0; $i -lt $ReplaceSeqences.Length;$i++)
+          {
+            $ReplaceByMatch = $ReplaceByMatch1[$i]
+            if ($contenuFichier -match $ReplaceByMatch)
+            {
+              $Match = $matches[1]
+              Write-Host "Replacement found : $ReplaceByMatch  : $Match" 
+              $FunctionBodyRep = $FunctionBodyRep.Replace($ReplaceSeqences[$i], $Match)
+            }
+            else
+            {
+              Write-Host "Replacement not found : $ReplaceByMatch" 
+            }
+          }
+          # Insérez les fonctions avant la fermeture de la classe
+          $contenuFichier = $contenuFichier.Insert($positionFermetureClasse, $FunctionBodyRep + "`n")
+
+          # Écrivez les modifications dans le fichier
+          $contenuFichier | Set-Content -Path $fichier.FullName -NoNewline
+          Write-Host "Fonctions ajoutées à la classe ou namespace $nomClasse dans le fichier $($fichier.FullName)"
+        }
+      }
+    }
+  }
+}
+
+# Fonction pour trouver la position de la fermeture de la classe
+function TrouverPositionFermetureClasse ($contenuFichier,$MatchBegin) {
+  $nombreAccoladesOuvrantes = 0
+  $nombreAccoladesFermantes = 0
+  $index = 0
+  $trouveClasse = $false
+  $positionFermeture = 0
+
+  # Parcourez le contenu du fichier ligne par ligne
+  $contenuFichier -split "`n" | ForEach-Object {
+
+      # Vérifiez si la ligne contient la déclaration de la classe
+      if ($trouveClasse -eq $false -and $_ -match  $MatchBegin) {
+          $trouveClasse = $true
+      }
+
+      # Si la classe a été trouvée, mettez à jour les compteurs d'accolades
+      if ($trouveClasse) {
+          $nombreAccoladesOuvrantes += ($_ -split "{").Count - 1
+          $nombreAccoladesFermantes += ($_ -split "}").Count - 1
+      }
+
+      # Si le nombre d'accolades fermantes est égal au nombre d'accolades ouvrantes
+      # pour la classe en cours, retournez l'index actuel
+      if ($trouveClasse -and $nombreAccoladesFermantes -gt 0 -and $nombreAccoladesFermantes -eq $nombreAccoladesOuvrantes -and $positionFermeture -eq 0) {
+          $positionFermeture = $index
+      }
+      $index += $_.Length + 1
+  }
+
+  # Retournez la dernière position si la classe n'a pas de fermeture explicite
+  return $positionFermeture
+}
+
+InsertFunctionInClass -Source $SourceFrontEnd -MatchBegin "class (\w+Service) extends CrudItemService" -FunctionBody @"
+    public clearAll(){
+        this.store.dispatch(FeatureEnginesActions.clearAll());
+    }
+    public clearCurrent(){
+      this._currentCrudItem = <Engine>{};
+      this._currentCrudItemId = 0;
+      this.store.dispatch(FeatureEnginesActions.clearCurrent());
+  }
+"@ -ReplaceSeqences @("Engines","Engine") -ReplaceByMatch1 @("\(Feature(\w+)Actions.loadAllByPost","class (\w+)Service extends CrudItemService") -NoMatchCondition "public clearAll\(\)"  -MatchCondition ""
+
+InsertFunctionInClass -Source $SourceFrontEnd -MatchBegin "export namespace (Feature\w+Actions)" -FunctionBody @"
+  
+  export const clearAll = createAction('[' + EngineCRUDConfiguration.storeKey +'] Clear all in state');
+  
+  export const clearCurrent = createAction('[' + EngineCRUDConfiguration.storeKey +'] Clear current');
+"@ -ReplaceSeqences @("Engine") -ReplaceByMatch1 @(" (\w+)CRUDConfiguration\.storeKey") -NoMatchCondition "export const clearAll" -MatchCondition "CRUDConfiguration\.storeKey"
 
 [string] $presentationApiFolder = GetPresentationApiFolder -Source $SourceBackEnd
 
