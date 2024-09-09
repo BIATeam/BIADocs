767c1fd62b0942f5cfb2f8b27afbd6ed9216e7c0
Gwenael Fillon <Gwenael.fillon@free.fr>
2024-09-09 17:13:36 +0200

    add search

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -106,6 +106,7 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+  plugins: [require.resolve('docusaurus-lunr-search')],
 };
 
 export default config;
